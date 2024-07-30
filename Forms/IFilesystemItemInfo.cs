4ff7260033f46f5ccd9ea2bdc6c1a210dae10853
Radek Veverka <radekg.veverka@gmail.com>
2024-07-30 09:43:17 +0200

    Enhanced filesystem browser validation - support for disabling particular (e.g. root) targets to be picked

--- Forms/IFilesystemItemInfo.cs
+++ Forms/IFilesystemItemInfo.cs
@@ -14,11 +14,11 @@ public class FileSystemItemInfo : IFilesystemItemInfo
     public string Path { get; set; } = null!;
     public string? Name { get; set; }
     public bool IsDirectory { get; set; }
-
-    public bool IsAccessible => true;
+    public bool IsAccessible { get; set; } = true;
+    public bool AllowPick { get; set; } = true;
 
     public override string ToString()
     {
-        return $"FSII{{Path: {Path}, Name: {Name}, IsDirectory: {IsDirectory}, IsAccessible: {IsAccessible}}}";
+        return $"FSII{{Path: {Path}, Name: {Name}, IsDirectory: {IsDirectory}, IsAccessible: {IsAccessible}, AllowPick: {AllowPick}}}";
     }
 }
