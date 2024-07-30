05d34be6b5daefc2e0d6b302d7483f26f04efca7
Radek Veverka <radekg.veverka@gmail.com>
2024-07-30 10:52:37 +0200

    Woring on entity merger

--- Utils/EfCoreEntityMerger.cs
+++ Utils/EfCoreEntityMerger.cs
@@ -13,8 +13,53 @@ public class RawSqlEntityMerger<TDbContext>(
     : IEntityMerger<TDbContext>
     where TDbContext : DbContext
 {
+    
+    
     public async Task MergeEntitiesAsync<TEntity>(TEntity from, TEntity to) where TEntity : class
     {
+        // Chatgpt 
+        using var dbContext = await dbContextFactory.CreateDbContextAsync();
+
+        var entityType = dbContext.Model.FindEntityType(typeof(TEntity)) ?? throw new InvalidOperationException();
+        var primaryKey = entityType.FindPrimaryKey() ?? throw new InvalidOperationException();
+        var primaryKeyProperty = primaryKey.Properties.First() ?? throw new InvalidOperationException();
+        var fromEntityKeyValue = primaryKeyProperty.PropertyInfo.GetValue(from);
+        var toEntityKeyValue = primaryKeyProperty.PropertyInfo.GetValue(to);
+
+        var foreignKeyProperties = dbContext.Model.GetEntityTypes()
+            .SelectMany(e => e.GetForeignKeys())
+            .Where(fk => fk.PrincipalEntityType.ClrType == typeof(TEntity))
+            .SelectMany(fk => fk.Properties)
+            .ToList();
+
+        await using var transaction = await dbContext.Database.BeginTransactionAsync();
+
+        try
+        {
+            foreach (var fkProperty in foreignKeyProperties)
+            {
+                var dependentEntityType = fkProperty.DeclaringEntityType.ClrType;
+                var tblName = dbContext.Model.FindEntityType(dependentEntityType).GetTableName();
+                var schema = "public";
+                var fkColumnName = fkProperty.GetColumnName();
+
+                var sql = $"""UPDATE {schema}"{tblName}" SET {fkColumnName} = @toEntityKeyValue WHERE {fkColumnName} = @fromEntityKeyValue""";
+                await dbContext.Database.ExecuteSqlRawAsync(sql, toEntityKeyValue, fromEntityKeyValue);
+            }
+
+            dbContext.Remove(from);
+            await dbContext.SaveChangesAsync();
+            await transaction.CommitAsync();
+        }
+        catch
+        {
+            await transaction.RollbackAsync();
+            throw;
+        }
+
+        return;
+        
+        
         if (from.Equals(to)) throw new ArgumentException("Entities being merged cannot be equal");
         
         var db = await dbContextFactory.CreateDbContextAsync();
@@ -39,7 +84,7 @@ public class RawSqlEntityMerger<TDbContext>(
 
                 var principalColName = fkc.PrincipalColumns.Single().Name;
                 var referencingColName = fkc.Columns.Single().Name;
-                var referencingTableName = tb.SchemaQualifiedName;
+                var referencingTableName = "public." + tb.SchemaQualifiedName; // TODO
                 var fromPkStr = fromPk.ToString();
                 var toPkStr = toPk.ToString();
                 // TODO - handle pk argument better
