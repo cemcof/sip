393dd524d28f3696c7d23a6a1e074c7de877ebe9
Radek Veverka <radekg.veverka@gmail.com>
2024-08-23 13:22:43 +0200

    Sample - improved equality handling

--- Experiments/Samples/Sample.cs
+++ Experiments/Samples/Sample.cs
@@ -4,41 +4,8 @@ using sip.Experiments.Model;
 
 namespace sip.Experiments.Samples;
 
-public class Sample : IStringFilter, IEquatable<Sample>
+public class Sample : IStringFilter, IIdentified<Guid>
 {
-    #region EQUALS_BOILERPLATE
-
-        public bool Equals(Sample? other)
-        {
-            if (ReferenceEquals(null, other)) return false;
-            if (ReferenceEquals(this, other)) return true;
-            return Id.Equals(other.Id);
-        }
-
-        public override bool Equals(object? obj)
-        {
-            if (ReferenceEquals(null, obj)) return false;
-            if (ReferenceEquals(this, obj)) return true;
-            if (obj.GetType() != this.GetType()) return false;
-            return Equals((Sample) obj);
-        }
-
-        public override int GetHashCode()
-        {
-            return Id.GetHashCode();
-        }
-
-        public static bool operator ==(Sample? left, Sample? right)
-        {
-            return Equals(left, right);
-        }
-
-        public static bool operator !=(Sample? left, Sample? right)
-        {
-            return !Equals(left, right);
-        }
-    #endregion
-
     public Guid Id { get; set; }
         
     [Required] public string Name { get; set; } = string.Empty;
