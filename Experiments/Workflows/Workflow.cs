e4b1dfacadb386e5e2c18dd186a4cdb2f01d4f98
Radek Veverka <radekg.veverka@gmail.com>
2024-08-23 13:27:03 +0200

    Refactored workflow equality handling

--- Experiments/Workflows/Workflow.cs
+++ Experiments/Workflows/Workflow.cs
@@ -2,42 +2,7 @@
 
 namespace sip.Experiments.Workflows;
 
-public class Workflow : IEquatable<Workflow>
-{
-    #region EQUALS
-        public bool Equals(Workflow? other)
-        {
-            if (ReferenceEquals(null, other)) return false;
-            if (ReferenceEquals(this, other)) return true;
-            return Title == other.Title && Provider == other.Provider;
-        }
-
-        public override bool Equals(object? obj)
-        {
-            if (ReferenceEquals(null, obj)) return false;
-            if (ReferenceEquals(this, obj)) return true;
-            if (obj.GetType() != this.GetType()) return false;
-            return Equals((Workflow) obj);
-        }
-
-        public override int GetHashCode()
-        {
-            return HashCode.Combine(Title, Provider);
-        }
-
-        public static bool operator ==(Workflow? left, Workflow? right)
-        {
-            return Equals(left, right);
-        }
-
-        public static bool operator !=(Workflow? left, Workflow? right)
-        {
-            return !Equals(left, right);
-        }
-    #endregion
-
-    
-
+public class Workflow : IIdentified<string> {
     public string Id { get; set; } = null!;
     public string Title { get; set; } = "<untitled workflow>";
     public string Description { get; set; } = "<no description>";
