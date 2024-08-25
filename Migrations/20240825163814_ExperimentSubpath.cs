08efd233f7163d921fd4675e2918fbad75ae1433
Radek <radek.veverka@icloud.com>
2024-08-25 18:38:48 +0200

    Supath migration

---
+++ Migrations/20240825163814_ExperimentSubpath.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace sip.Migrations
+{
+    /// <inheritdoc />
+    public partial class ExperimentSubpath : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "SubPath",
+                table: "ExperimentStorage",
+                type: "text",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "SubPath",
+                table: "ExperimentStorage");
+        }
+    }
+}
